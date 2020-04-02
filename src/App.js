ad12e1a96086ac882140dd3c930b0cad7ee81d08
gawaxx <jules.blanc@gmail.com>
2020-04-02 23:04:44 +0100

    creating footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
           <Stats />
       </header>
+      <footer className="App-footer" >
+          <h1> this is the footer</h1>
+      </footer>
     </div>
   );
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -42,7 +42,7 @@ export default class Stats extends Component {
         const { deaths, cases, recovered, currently_infected } = this.state
         return (
             <div>
-                <h1> Here are the latest global statistics regarding the pandemic of COVID-19 </h1>
+                <h1> Here is the latest global statistics regarding the pandemic of COVID-19 </h1>
                 <h2 style={{ color: 'white' }} > {this.transformNumber(currently_infected)} active cases </h2>
                 <h2 style={{ color: 'white' }}> {this.transformNumber(cases)} cases  </h2>
                 <h2 style={{ color: 'red' }}> {this.transformNumber(deaths)} deaths </h2>
