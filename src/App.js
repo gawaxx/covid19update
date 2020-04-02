fd3d25b45f8ed9247638c7856e4b61c0ada67f56
gawaxx <jules.blanc@gmail.com>
2020-04-02 21:00:00 +0100

    spliting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Stats from './components/Stats';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+          <Stats />
       </header>
     </div>
   );
---
+++ src/components/Stats.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Stats extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Here are the latest statistics regarding CoVid-19</h1>
+            </div>
+        )
+    }
+}
