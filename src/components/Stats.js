a9610aa10167e8867e89d35a22e05ebe6e24f1be
gawaxx <jules.blanc@gmail.com>
2020-04-02 21:32:17 +0100

    Added recovered and changed title

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,22 +5,24 @@ export default class Stats extends Component {
 
     state = {
         deaths: [],
-        cases: []
+        cases: [],
+        recovered: []
     }
 
     componentDidMount() {
-        API.GetAPI(APILINK + "Allreports").then( data => this.setState( {deaths: data.reports[0].deaths, cases: data.reports[0].cases} ) )
+        API.GetAPI(APILINK + "Allreports").then( data => this.setState( {deaths: data.reports[0].deaths, cases: data.reports[0].cases, recovered: data.reports[0].recovered } ) )
         // API.GetAPI(APILINK + "Allreports").then( data => console.log(data.reports[0].cases))
 
     }
 
     render() {
-        const { deaths, cases } = this.state
+        const { deaths, cases, recovered } = this.state
         return (
             <div>
-                <h1> Here are the latest statistics regarding CoVid-19</h1>
-                <h2 style={{color: 'white'}}> {cases} total cases  </h2>
-                <h2 style={{color: 'red'}}> {deaths} total deaths </h2>
+                <h1> Here are the latest statistics regarding COVID-19</h1>
+                <h2 style={{ color: 'white' }}> {cases} total cases  </h2>
+                <h2 style={{ color: 'red' }}> {deaths} total deaths </h2>
+                <h2 style={{ color: 'green' }} > {recovered} total recovered </h2>
             </div>
         )
     }
