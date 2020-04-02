dbae6f5961d37e5bee74107beef20518b9ec99a3
gawaxx <jules.blanc@gmail.com>
2020-04-02 23:43:08 +0100

    Footer pretty much done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
+            <div style={{ fontSize: '20px'}}>
 
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/gawaxx/covid19update" > Github </a>
+                <a style={{ color: 'white' }} target="_blank" rel="noopener noreferrer" href="https://github.com/gawaxx/covid19update" > Github </a>
+                <br></br>
+                <a style={{ color: 'white' }} target="_blank" rel="noopener noreferrer" href="http://www.linkedin.com/in/jules-blanc-29a36b179" > LinkedIn </a>
 
-                <a target="_blank" rel="noopener noreferrer" href="http://www.linkedin.com/in/jules-blanc-29a36b179" > LinkedIn </a>
-
-                <p>
+                <p style={{ color: 'white' }}>
                     COVID19Update | made with <span role="img" aria-label="baguette"> 🥖 </span> and ReactJS by Jules Blanc, 2020.
                 </p>
 
