b5df663203adf3f3c01c47856f9c09ba9fab734e
gawaxx <jules.blanc@gmail.com>
2020-04-02 21:25:16 +0100

    Making API requests

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,1 +1,7 @@
 // this is the file with all the API calls
+
+export const APILINK = "https://covid19-server.chrismichael.now.sh/api/v1/"
+
+export const GetAPI = (url) => fetch(url).then(res => res.json())
+
+export const API = { GetAPI }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import { API, APILINK } from '../adapters/API.js'
 
 export default class Stats extends Component {
+
+    state = {
+        deaths: []
+    }
+
+    componentDidMount() {
+        API.GetAPI(APILINK + "Allreports").then( data => this.setState( {deaths: data.reports[0].deaths} ) )
+        // API.GetAPI(APILINK + "Allreports").then( data => console.log(data.reports[0].cases))
+
+    }
+
     render() {
         return (
             <div>
                 <h1> Here are the latest statistics regarding CoVid-19</h1>
+                <h2 style={{color: 'red'}}> {this.state.deaths} total deaths </h2>
             </div>
         )
     }
