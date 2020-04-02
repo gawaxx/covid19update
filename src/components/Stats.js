45f1b7d6735f23760a6d578ec53f9cdcc205d7a3
gawaxx <jules.blanc@gmail.com>
2020-04-02 21:29:08 +0100

    Adding total cases

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,20 +4,23 @@ import { API, APILINK } from '../adapters/API.js'
 export default class Stats extends Component {
 
     state = {
-        deaths: []
+        deaths: [],
+        cases: []
     }
 
     componentDidMount() {
-        API.GetAPI(APILINK + "Allreports").then( data => this.setState( {deaths: data.reports[0].deaths} ) )
+        API.GetAPI(APILINK + "Allreports").then( data => this.setState( {deaths: data.reports[0].deaths, cases: data.reports[0].cases} ) )
         // API.GetAPI(APILINK + "Allreports").then( data => console.log(data.reports[0].cases))
 
     }
 
     render() {
+        const { deaths, cases } = this.state
         return (
             <div>
                 <h1> Here are the latest statistics regarding CoVid-19</h1>
-                <h2 style={{color: 'red'}}> {this.state.deaths} total deaths </h2>
+                <h2 style={{color: 'white'}}> {cases} total cases  </h2>
+                <h2 style={{color: 'red'}}> {deaths} total deaths </h2>
             </div>
         )
     }
